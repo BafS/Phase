6685a5f86ddbc3f8781324ec2ee6af9ed24d109c
BafS <fabacrans@gmail.com>
2019-04-30 01:31:10 +0200

    Add package and eslint
    And clean the code

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: "airbnb-base",
+    rules: {
+        quotes: ["warn", "single"],
+        radix: 0,
+        'no-plusplus': 0,
+        'no-param-reassign': 0,
+        'no-new-func': 0,
+    },
+    "env": {
+        "browser": true,
+        // "node": true
+    }
+};
