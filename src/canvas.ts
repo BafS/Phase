1607f77a6a90836fbb573ab9de2bf9bb87799a6c
BafS <fabacrans@gmail.com>
2019-12-02 02:14:37 +0100

    Update design

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,17 +1,20 @@
 const colors = {
-  function: 'rgba(96, 148, 188, .9)',
+  // function: 'rgba(96, 148, 188, .9)',
+  function: 'rgba(60, 169, 248, .8)',
   // function: '#5a83b2',
-  overflow: 'rgba(255, 0, 0, .8)',
-  strokeStyle: 'rgba(200, 200, 200, .2)',
-  extrema: {
-    top: 'rgba(255, 0, 0, .15)',
-    bottom: 'rgba(0, 255, 0, .15)',
+  overflow: 'rgba(255, 0, 0, .75)',
+  strokeStyle: 'rgba(200, 200, 200, .25)',
+  text: 'rgba(200, 200, 200, .9)',
+  peaks: {
+    top: 'rgba(255, 50, 50, .2)',
+    bottom: 'rgba(50, 255, 50, .2)',
   },
 };
 
 const options = {
-  lineDash: [5, 3],
-  font: '11px monospace',
+  lineDash: [3, 6],
+  lineWidth: 1,
+  font: '12px monospace',
 };
 
 /**
@@ -27,41 +30,44 @@ function plot(points: Float32Array, canvas: HTMLCanvasElement, clear: boolean = 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  ctx.lineWidth = 1;
+  const canvasWidth = canvas.width - 1;
+  const canvasHeight = canvas.height - 1;
+
+  ctx.lineWidth = options.lineWidth;
   ctx.strokeStyle = colors.strokeStyle;
   // x axis
-  const halfHeight = canvas.height / 2;
+  const halfHeight = canvasHeight / 2;
   // TODO: Stroke width ?
   const xAxisNum = 10;
   ctx.setLineDash(options.lineDash);
-  ctx.lineWidth = 0.5;
   for (let i = 1; i < xAxisNum; ++i) {
     ctx.beginPath();
-    ctx.moveTo(0, Math.round(i * (halfHeight / xAxisNum * 2)));
-    ctx.lineTo(canvas.width, 0.5 + Math.round(i * (halfHeight / xAxisNum * 2)));
+    const y = Math.round(i * (halfHeight / xAxisNum * 2));
+    ctx.moveTo(0, 0.5 + y);
+    ctx.lineTo(canvasWidth, 0.5 + y);
     ctx.stroke();
   }
 
-  ctx.fillStyle = 'rgba(200, 200, 200, .1)';
-  ctx.fillRect(0, halfHeight, canvas.width, 1); // 0 x axis
+  ctx.fillStyle = 'rgba(100, 100, 100, .5)';
+  ctx.fillRect(0, halfHeight, canvasWidth, 1); // 0 x axis
 
   ctx.strokeStyle = colors.function;
   ctx.setLineDash([]);
   ctx.beginPath();
-  ctx.lineWidth = 1.5;
-  const ratioX = (canvas.width + 1) / points.length;
-  const ratioY = -canvas.height / 2;
+  ctx.lineWidth = options.lineWidth * 1.5;
+  const ratioX = (canvasWidth + 1) / points.length;
+  const ratioY = -canvasHeight / 2;
 
   let previous: number[]|null = null;
-  const extrema = [Infinity, -Infinity];
+  const peaks = [Infinity, -Infinity];
   points.forEach((y: number, x: number): void => {
     y -= 1;
 
-    // Get extrema
-    if (y > extrema[1]) {
-      extrema[1] = y;
-    } else if (y < extrema[0]) {
-      extrema[0] = y;
+    // Get amplitude peaks
+    if (y > peaks[1]) {
+      peaks[1] = y;
+    } else if (y < peaks[0]) {
+      peaks[0] = y;
     }
 
     x *= ratioX;
@@ -72,7 +78,7 @@ function plot(points: Float32Array, canvas: HTMLCanvasElement, clear: boolean = 
       ctx.fillRect(x - 1, 0, 1, 3);
     } else if (previous && y < -2) {
       ctx.fillStyle = colors.overflow;
-      ctx.fillRect(x - 1, canvas.height, 1, -3);
+      ctx.fillRect(x - 1, canvasHeight, 1, -3);
     }
 
     y *= ratioY;
@@ -86,11 +92,16 @@ function plot(points: Float32Array, canvas: HTMLCanvasElement, clear: boolean = 
   });
   ctx.stroke();
 
-  // Extrema
-  ctx.fillStyle = colors.extrema.bottom;
-  ctx.fillRect(0, Math.round(extrema[0] * ratioY), canvas.width, 1); // max
-  ctx.fillStyle = colors.extrema.top;
-  ctx.fillRect(0, Math.round(extrema[1] * ratioY), canvas.width, 1); // min
+  // Amplitude peaks
+  ctx.fillStyle = colors.text;
+  const peakAmpTop = Math.floor(peaks[1] * ratioY);
+  const peakAmpBottom = Math.ceil(peaks[0] * ratioY);
+  ctx.fillText(' Û', canvasWidth - 20, Math.max(15, peakAmpTop - 6));
+  ctx.fillText('-Û', canvasWidth - 20, Math.min(canvasHeight - 8, peakAmpBottom + 15));
+  ctx.fillStyle = colors.peaks.bottom;
+  ctx.fillRect(0, peakAmpTop, canvasWidth, 1); // max
+  ctx.fillStyle = colors.peaks.top;
+  ctx.fillRect(0, peakAmpBottom, canvasWidth, 1); // min
 
   return canvas;
 }
@@ -112,14 +123,14 @@ function plotAudioBuffer(buffer: AudioBuffer, canvas: HTMLCanvasElement): HTMLCa
   const inc = 1000 / (10 ** Math.floor(Math.log(ratio * 2) * Math.LOG10E + 1));
   const markerSize = 25;
 
-  ctx.strokeStyle = options.strokeStyle;
-  ctx.lineWidth = 1;
+  ctx.strokeStyle = colors.strokeStyle;
+  ctx.lineWidth = options.lineWidth;
   ctx.setLineDash(options.lineDash);
 
   for (let i = 0; i < duration; i += inc) {
     const x = Math.round(i * ratio);
     // console.log(buffer.duration, i, ratio);
-    ctx.fillStyle = 'rgba(200, 200, 200, .6)';
+    ctx.fillStyle = colors.text;
     ctx.fillText(`${i}ms`, (i * ratio) + 4, canvas.height - 5);
     if (i > 0) {
       ctx.beginPath();
