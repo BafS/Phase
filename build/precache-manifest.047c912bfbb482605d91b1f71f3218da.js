2e3c4952b83fa8fd05790b643a02620b6baf5309
BafS <fabacrans@gmail.com>
2019-12-05 02:31:23 +0100

    Add build

---
+++ build/precache-manifest.047c912bfbb482605d91b1f71f3218da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6020220624ee9c057128391ad61542d3",
+    "url": "/BafS/Equations-Player/editor.worker.js"
+  },
+  {
+    "revision": "1955778aad23195fa356a1e0b3c34c39",
+    "url": "/BafS/Equations-Player/index.html"
+  },
+  {
+    "revision": "1b0b37f277c6f1f686a2",
+    "url": "/BafS/Equations-Player/static/css/2.430a3d82.chunk.css"
+  },
+  {
+    "revision": "d47eb84013c7815bdf66",
+    "url": "/BafS/Equations-Player/static/css/main.e0d3e8a9.chunk.css"
+  },
+  {
+    "revision": "1b0b37f277c6f1f686a2",
+    "url": "/BafS/Equations-Player/static/js/2.6cc84886.chunk.js"
+  },
+  {
+    "revision": "d35c99a7c86d1cdcf72b",
+    "url": "/BafS/Equations-Player/static/js/3.bb302cf7.chunk.js"
+  },
+  {
+    "revision": "2cdb7819d5833636d444",
+    "url": "/BafS/Equations-Player/static/js/4.0e849903.chunk.js"
+  },
+  {
+    "revision": "7babc3d4a80eb5e7eab0",
+    "url": "/BafS/Equations-Player/static/js/5.0086aefb.chunk.js"
+  },
+  {
+    "revision": "d47eb84013c7815bdf66",
+    "url": "/BafS/Equations-Player/static/js/main.b589feb2.chunk.js"
+  },
+  {
+    "revision": "d8f237176c9abb1d9018",
+    "url": "/BafS/Equations-Player/static/js/runtime-main.e5232d4c.js"
+  },
+  {
+    "revision": "8c8676463a34b663b2e655088c36512d",
+    "url": "/BafS/Equations-Player/typescript.worker.js"
+  }
+]);
