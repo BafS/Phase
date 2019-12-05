7185bfd7b4f9317037c4385c0368b4ee247a46fa
BafS <fabacrans@gmail.com>
2019-12-05 02:21:54 +0100

    Add hotkeys for all actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React, {
   useEffect,
   BaseSyntheticEvent,
   useLayoutEffect,
+  useCallback,
 } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
@@ -58,17 +59,22 @@ const Player: React.FC<{
     return (): void => source && source.stop();
   }, [isPlaying]);
 
-  useKeyboardShortcut(['Shift', 'P'], useCallback((): void => {
+  useKeyboardShortcut(['Control', 'S'], useCallback((): void => {
     setIsPlaying((prevPlaying): boolean => !prevPlaying);
   }, [setIsPlaying]));
 
   return (
-    <>
+    <div className="player">
       <Plot buffer={buffer} options={{ width, height }} />
-      <Button handleClick={(): void => {
-        setIsPlaying((p): boolean => !p);
-      }}>{isPlaying ? 'stop' : 'play'}</Button>
-    </>
+      <div className="panel">
+        <span className="text-dark text-sm">Length: {buffer.duration}s | </span>
+        <span className="text-dark text-sm">Frequency: {buffer.sampleRate}hz | </span>
+        <Button className="btn btn-sm" handleClick={(): void => {
+          setIsPlaying((p): boolean => !p);
+        }}>{isPlaying ? 'stop' : 'play'}</Button>
+        <span className="text-dark text-sm"> (Ctrl+S)</span>
+      </div>
+    </div>
   );
 };
 
@@ -101,19 +107,24 @@ const PlotThumb: React.FC<PlotThumb> = ({ buffer, code, stateCallback }): JSX.El
 );
 
 const App: React.FC = (): JSX.Element => {
+  let monaco: editor.IStandaloneCodeEditor;
+
   const [codeState, setCode] = useState<string>([
-    'const a = 440',
+    'const a = 440;',
     'const sum = (fn, [min, max]) => Array(max - min + 1).fill().reduce((sum, _, i) => sum + fn(i + min), 0);',
     '',
-    '// return (-1) ** Math.floor(2*440*t) // square',
+    '// Main waveforms',
+    'Math.sin(440 * t * Math.PI * 2); // sine',
+    '(-1) ** Math.floor(2 * 440 * t); // square',
     // '// return 2 * (2*Math.floor(440*t) - Math.floor(2*440*t)) + 1 // square',
     // return Math.abs(t * 440 % 4 - 2) - 1 // triangle
-    '// return 4 * Math.abs(t*440 - Math.round(t*440)) - 1 // triangle',
-    '// return (t*440 - Math.round(t*440)) * 2 // sawtooth',
+    '(440 * t - Math.round(440 * t)) * 2; // sawtooth',
+    '4 * Math.abs(440 * t - Math.round(t*440)) - 1; // triangle',
     // '// return (t*440 - Math.floor(.5 + t*440)) * 2 // sawtooth',
     // '// Math.sin(440 * Math.log10(t) * Math.PI * 2)',
-    'return Math.sin(440 * t * Math.PI * 2) * 1',
-    '// return sum(s => Math.sin(440 * t * Math.PI * 2 * s) / 2, [1, 3])',
+    // '// Other',
+    // 'Math.sin(440 * t * Math.PI * 2) * .2; // reduce amplitude',
+    // 'sum(s => Math.sin(440 * t * Math.PI * 2 * s) / 2, [1, 3]);',
   ].join('\n'));
   const [bufferState, setBuffer] = useState<AudioBuffer|null>(null);
   // const [plotStateList, setPlotStateList] = useState<PlotState[]>([]);
@@ -138,14 +149,63 @@ const App: React.FC = (): JSX.Element => {
 
   const handlePeriodChange = (event: BaseSyntheticEvent): void => setPeriod(+event.target.value);
 
-  const editorDidMount = (monaco: editor.IEditor): void => monaco.focus();
+  const editorDidMount = (monacoMount: editor.IStandaloneCodeEditor): void => {
+    monaco = monacoMount;
+    monaco.focus();
+  };
 
   const debounce = useDebounce((): void => setWidth(window.innerWidth), 500);
+
+  const doProcess = (code: string): void => {
+    const lines = code.trim().split('\n');
+    let last = lines.pop();
+    if (!last) {
+      return;
+    }
+
+    if (!last.trimStart().startsWith('return') && !last.trimStart().startsWith('//')) {
+      last = `return ${last}`; // Last line
+    }
+
+    const finalCode = lines.concat(last).join('\n');
+
+    try {
+      const audioBuffer = process(finalCode, period, duration);
+      setBuffer(audioBuffer);
+      setErrorCode('');
+    } catch (e) {
+      setErrorCode(e.toString());
+      console.error(e);
+    }
+  };
+
+  const handleProcess = (): void => doProcess(codeState);
+
+  const handleProcessLine = (): void => {
+    if (!monaco) {
+      return;
+    }
+
+    const position = monaco.getPosition();
+    if (position === null) {
+      return;
+    }
+
+    const { lineNumber } = position;
+
+    const lines = monaco.getValue().trim().split('\n').slice(0, lineNumber);
+
+    doProcess(lines.join('\n'));
+  };
 
   useLayoutEffect((): () => void => {
     window.addEventListener('resize', debounce);
     return (): void => window.removeEventListener('resize', debounce);
   });
+
+  useKeyboardShortcut(['Control', '1'], useCallback((): void => handleProcess(), [codeState]));
+
+  useKeyboardShortcut(['Control', '2'], useCallback((): void => handleProcessLine(), []));
 
   return (
     <div className="App">
@@ -155,7 +215,7 @@ const App: React.FC = (): JSX.Element => {
           {bufferState
             ? <Player buffer={bufferState} options={{ width, height: 350 }}/>
             // ? <Plot buffer={bufferState.getChannelData(0)} options={{width, height: 350}}/>
-            : ''}
+            : <span className="text-dark text-sm">(click on "process" to generate and plot the audio)</span>}
         </div>
 
         <div className="main-panel">
@@ -167,22 +227,16 @@ const App: React.FC = (): JSX.Element => {
           <Button handleClick={(): void => {
           }}>Play loop</Button> */}
 
-          <Button handleClick={(): void => {
-            // if (bufferState === null) {
-            try {
-              const audioBuffer = process(codeState, period, duration);
-              setBuffer(audioBuffer);
-              setErrorCode('');
-            } catch (e) {
-              setErrorCode(e.toString());
-            }
-          }}>render</Button>
+          <div className="right">
+            <Button handleClick={handleProcess}>process code</Button>
+            <span className="text-dark text-sm"> (Ctrl+1)</span><br/>
+            <span className="text-dark text-sm">process line: (Ctrl+2)</span>
+          </div>
 
           <span>
             <em>t<sub>max</sub></em>:
             <InputNumber min={0} max={100000} defaultValue={period} handleChange={handlePeriodChange} />ms
           </span><br/>
-          {/* <span>loop:</span> */}
         </div>
         {/*
         {plotStateList !== []
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Button: React.FC<{
   handleClick: () => void;
-}> = React.forwardRef(({ handleClick, children }, ref): JSX.Element => (
-  <button className="btn" onClick={handleClick}>{children}</button>
+  [key: string]: any;
+}> = React.forwardRef(({ handleClick, children, ...props }, ref): JSX.Element => (
+  <button className="btn" {...props} onClick={handleClick}>{children}</button>
 ));
 
 export default Button;
